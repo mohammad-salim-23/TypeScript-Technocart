eb4976cb61b40b3d6e7e3033063136ca42114dba
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-11-06 23:27:43 +0600

    oop static

---
+++ module3/3.6.ts
@@ -0,0 +1,37 @@
+{
+    // access modifiers
+    class  BankAccount{
+        public readonly id:number;
+        public name:string;
+        // private _balance:number;// eita re bahir teke keu access korte parbe na
+        protected _balance:number;//jodi kokono emon hoy amar child class extends kortece ei class ke and amar ei property o ami chai child class jate access korte pare tokon private er poriborte public use kora better
+
+        constructor(id:number,name:string,balance:number){
+            this.id=id;
+            this.name=name;;
+            this._balance=balance;
+        }
+
+        // addDeposite(amount:number){
+        //     this._balance=this._balance+amount;
+        // }
+        // setter
+        set deposit(amount:number){
+            this._balance=this._balance+amount;
+        }
+        // getBalance(){
+        //     return this._balance;
+        // }
+        // getter
+        get Balance(){
+            return this._balance;
+        }
+    }
+    const goribManusherAccount = new BankAccount(111,"Mr.Amir Hamja",20);
+    // // goribManusherAccount._balance=0; i don't access this property because it's a private
+    // goribManusherAccount.addDeposite(59);
+      goribManusherAccount.deposit = 10;
+    // const hamjarBalance = goribManusherAccount.getBalance();
+    const myBalance = goribManusherAccount.Balance;
+    console.log(myBalance);
+}
